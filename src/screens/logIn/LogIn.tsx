df4780e171187cad7085bce9f1cf30dbcf57ac86
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-22 09:37:40 +0530

    Added login screen

--- src/screens/logIn/LogIn.tsx
+++ src/screens/logIn/LogIn.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+import { LOGIN_BACK_GROUND_IMAGE, NETFLIX_LOGO } from "../../utils/constants";
+
 const LogIn = () => {
   return (
     <div>
-      <h1>Log In</h1>
+      <img src={NETFLIX_LOGO} alt="logo" className="absolute w-44" />
+      <div className="absolute bg-gray-700 py-12 w-4/12 mx-auto right-0 left-0 my-36 rounded-sm">
+        <div className="w-3/5 mx-auto right-0 left-0">
+          <h1 className="text-white font-bold text-3xl mb-4">Sign In</h1>
+          <form className="flex flex-col">
+            <input
+              placeholder="Email"
+              type="email"
+              className="p-2 my-4 rounded-sm"
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              className="p-2 my-4 rounded-sm"
+            />
+            <div className="my-4">
+              <button className="bg-red-600 py-2 rounded-sm text-white font-semibold flex justify-center w-full">
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <img src={LOGIN_BACK_GROUND_IMAGE} alt="login_image" />
     </div>
   );
 };
