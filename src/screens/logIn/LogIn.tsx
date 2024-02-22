4ad4d5687a272bc3ecefecc2a6e60c9b8c8c23a7
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-22 12:41:20 +0530

    Added Sig up UI

--- src/screens/logIn/LogIn.tsx
+++ src/screens/logIn/LogIn.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { LOGIN_BACK_GROUND_IMAGE, NETFLIX_LOGO } from "../../utils/constants";
 
 const LogIn = () => {
+  const [isSignInForm, setIsSignInForm] = useState(false);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <img src={NETFLIX_LOGO} alt="logo" className="absolute w-44" />
       <div className="absolute bg-black py-12 w-3/12 mx-auto right-0 left-0 my-36 rounded-sm bg-opacity-75">
         <div className="w-3/5 mx-auto right-0 left-0">
-          <h1 className="text-white font-bold text-3xl mb-4">Sign In</h1>
+          <h1 className="text-white font-bold text-3xl mb-4">
+            {isSignInForm ? "Sign Up" : "Sign In"}
+          </h1>
           <form className="flex flex-col text-white">
+            {isSignInForm && (
+              <input
+                placeholder="Full Name"
+                type="text"
+                className="p-2 my-4 bg-gray-700 bg-opacity-50 rounded-sm"
+              />
+            )}
             <input
               placeholder="Email"
               type="email"
@@ -22,8 +37,15 @@ const LogIn = () => {
             />
             <div className="my-4">
               <button className="bg-red-600 py-2 rounded-sm text-white font-semibold flex justify-center w-full">
-                Sign In
+                {isSignInForm ? "Sign Up" : "Sign In"}
               </button>
+              <div onClick={toggleSignInForm} className="mt-4 cursor-pointer">
+                <p>
+                  {isSignInForm
+                    ? "Already Registered. Sign in here."
+                    : "New to Netflix? Sign up now."}
+                </p>
+              </div>
             </div>
           </form>
         </div>
