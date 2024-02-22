1331ebb283567c247fe44e0a230725b489527a09
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-22 11:35:33 +0530

    Polishing the UI

--- src/screens/logIn/LogIn.tsx
+++ src/screens/logIn/LogIn.tsx
@@ -6,19 +6,19 @@ const LogIn = () => {
   return (
     <div>
       <img src={NETFLIX_LOGO} alt="logo" className="absolute w-44" />
-      <div className="absolute bg-gray-700 py-12 w-4/12 mx-auto right-0 left-0 my-36 rounded-sm">
+      <div className="absolute bg-black py-12 w-3/12 mx-auto right-0 left-0 my-36 rounded-sm bg-opacity-75">
         <div className="w-3/5 mx-auto right-0 left-0">
           <h1 className="text-white font-bold text-3xl mb-4">Sign In</h1>
-          <form className="flex flex-col">
+          <form className="flex flex-col text-white">
             <input
               placeholder="Email"
               type="email"
-              className="p-2 my-4 rounded-sm"
+              className="p-2 my-4 bg-gray-700 bg-opacity-50 rounded-sm"
             />
             <input
               placeholder="Password"
               type="password"
-              className="p-2 my-4 rounded-sm"
+              className="p-2 my-4 bg-gray-700 bg-opacity-50 rounded-sm"
             />
             <div className="my-4">
               <button className="bg-red-600 py-2 rounded-sm text-white font-semibold flex justify-center w-full">
