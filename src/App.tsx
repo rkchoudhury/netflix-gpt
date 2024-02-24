bda690d5a8c8c5c2959f7b294ef0c0b98eee2157
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-24 08:16:41 +0530

    Moving the provider wrapper code back to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import appRouter from "./navigation/AppNavigator";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={appRouter} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
   );
 }
 
