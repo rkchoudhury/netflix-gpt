fc310803ee612f03cc525617c051caf5601dc1b2
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-25 11:48:26 +0530

    Added secondary container contents

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { IMovie } from "../model/movieModel";
+import { CDN_IMAGE_URL } from "../utils/constants";
+
+interface IProps {
+  movie: IMovie;
+}
+
+const MovieCard = ({ movie }: IProps) => {
+  const { poster_path, title } = movie;
+  return (
+    <div className="w-36 mr-4">
+      <img src={`${CDN_IMAGE_URL}${poster_path}`} alt={title} />
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/screens/browse/SecondaryContainer.tsx
+++ src/screens/browse/SecondaryContainer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import MovieList from "../../components/MovieList";
+import { IRootState } from "../../model/RootState";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer Container</div>;
+  const { nowPlayingMovie } = useSelector((state: IRootState) => state.movies);
+  return (
+    <div className="bg-black">
+      <div className="-mt-52 relative z-20">
+        {nowPlayingMovie?.length && (
+          <MovieList title={"Now Playing"} movies={nowPlayingMovie} />
+        )}
+        {nowPlayingMovie?.length && (
+          <MovieList title={"Now Playing"} movies={nowPlayingMovie} />
+        )}
+        {nowPlayingMovie?.length && (
+          <MovieList title={"Now Playing"} movies={nowPlayingMovie} />
+        )}
+        {nowPlayingMovie?.length && (
+          <MovieList title={"Now Playing"} movies={nowPlayingMovie} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
