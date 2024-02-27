0d293bfdffec6cadcb0d2bfae6a4075e7780b1d8
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-02-27 21:58:37 +0530

    Added error screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,16 +3,19 @@ import { createBrowserRouter } from "react-router-dom";
 
 import LogIn from "../screens/logIn/LogIn";
 import Browse from "../screens/browse/Browse";
+import Error from "../screens/error/Error";
 import { ROUTE_NAMES } from "./Routes";
 
 const appRouter = createBrowserRouter([
   {
     path: ROUTE_NAMES.HOME,
     element: <LogIn />,
+    errorElement: <Error />,
   },
   {
     path: ROUTE_NAMES.BROWSE,
     element: <Browse />,
+    errorElement: <Error />,
   },
 ]);
 
